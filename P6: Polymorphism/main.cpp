8520e347bc8fe5ea7d73a21a470088e683a9a0f7
UnknwnDev <sambugua45@gmail.com>
2022-03-04 13:23:03 -0800

    P6: Polymorphism Step 2

--- P6: Polymorphism/main.cpp
+++ P6: Polymorphism/main.cpp
@@ -1,8 +1,46 @@
 #include<iostream>
 
+class Shape{
 
+};
+
+class Rectangle{
+    private:
+        double _width;
+        double _height;
+    public:
+        Rectangle(double width, double height);
+};
+
+class Square{
+    public:
+        Square(double size);  
+};
+
+class Circle{
+    public:
+        Circle(double radius);  
+};
 
 int main(){
-    std::cout << "Hello World\n";
+    Rectangle r1{ 3.0, 5.0 }; 
+    Square s1{ 4.0 }; 
+    Circle c1{ 10.0 }; 
+
+    // printArea(r1); 
+    // printArea(s1); 
+    // printArea(c1); 
     return 0;
+}
+
+Rectangle::Rectangle(double width, double height){
+    
+}
+
+Square::Square(double size){
+
+}
+
+Circle::Circle(double radius){
+
 }
